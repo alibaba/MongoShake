73f94dde2a6cb611b63a1edd710ec61d6e26867a
ManleyLiu <daywbdb@qq.com>
2018-09-19 13:05:01 +0800

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -4,7 +4,7 @@ if [ $# != 1 ] ; then
     echo "USAGE: $0 [pidfile]"
     exit 0
 fi
-ppid=$(ps -ef | grep -v grep |awk '{if ($2=='`cat $1`') print $3}')
+ppid=$(ps -ef | awk '{if ($2=='`cat $1`') print $3}')
 [ -z $ppid ] && echo "[Fail] No process number for $(cat "$1")." && exit 1
 if [ $ppid -eq 1 ];then
     kill -9 "$(cat "$1")"
