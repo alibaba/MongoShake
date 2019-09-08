4dfe433c56409e288071971038615c4486fee99e
zengyuekang <zengyuekang@jshuii.com>
2019-09-08 09:42:28 +0800

    build.sh 支持跨平台编译,如在windows下编译linux可执行文件

--- build.sh
+++ build.sh
@@ -7,14 +7,25 @@ modules=(collector receiver)
 
 tags=""
 
+#whether to compile across platforms
+#for example: compile Linux executables on MAC or compile Linux 64-bit executables on Windows
+is_cross_compile=false
+if [ $is_cross_compile = true ]; then
+    #cross platform cross compilation
+    export CGO_ENABLED=0
+    export GOARCH=amd64
+    #GOOS optional: windows linux darwin
+    export GOOS=linux
+fi
+
 # older version Git don't support --short !
-if [ -d ".git" ]; then
-  #branch=`git symbolic-ref --short -q HEAD`
-  branch=$(git symbolic-ref -q HEAD | awk -F'/' '{print $3;}')
-  cid=$(git rev-parse HEAD)
+if [ -d ".git" ];then
+    #branch=`git symbolic-ref --short -q HEAD`
+    branch=$(git symbolic-ref -q HEAD | awk -F'/' '{print $3;}')
+    cid=$(git rev-parse HEAD)
 else
-  branch="unknown"
-  cid="0.0"
+    branch="unknown"
+    cid="0.0"
 fi
 branch=$branch","$cid
 
@@ -32,17 +43,17 @@ export GOPATH
 #compile_line='-race'
 compile_line=''
 if [ -z "$DEBUG" ]; then
-  DEBUG=0
+    DEBUG=0
 fi
 
 info="mongoshake/common.BRANCH=$branch"
 # inject program information about compile
 if [ $DEBUG -eq 1 ]; then
-  echo "[ BUILD DEBUG ]"
-  info=$info',debug'
+	echo "[ BUILD DEBUG ]"
+	info=$info',debug'
 else
-  echo "[ BUILD RELEASE ]"
-  info=$info",release"
+	echo "[ BUILD RELEASE ]"
+	info=$info",release"
 fi
 
 # golang version
@@ -50,34 +61,30 @@ goversion=$(go version | awk -F' ' '{print $3;}')
 info=$info","$goversion
 bigVersion=$(echo $goversion | awk -F'[o.]' '{print $2}')
 midVersion=$(echo $goversion | awk -F'[o.]' '{print $3}')
-if [ $bigVersion -lt "1" -o $bigVersion -eq "1" -a $midVersion -lt "9" ]; then
-  echo "go version[$goversion] must >= 1.9"
-  exit 1
+if  [ $bigVersion -lt "1" -o $bigVersion -eq "1" -a $midVersion -lt "9" ]; then
+    echo "go version[$goversion] must >= 1.9"
+    exit 1
 fi
 
 t=$(date "+%Y-%m-%d_%H:%M:%S")
 info=$info","$t
 
-#cross platform cross compilation
-export CGO_ENABLED=0
-export GOARCH=amd64
-#GOOS optional: windows linux darwin
-export GOOS=linux
-
 run_builder='go build -v'
 
-for i in "${modules[@]}"; do
-  echo "Build ""$i"
-  if [ $DEBUG -eq 1 ]; then
-    $run_builder ${compile_line} -ldflags "-X $info" -gcflags='-N -l' -o "bin/$i" -tags "debug" "src/mongoshake//$i/main/$i.go"
-  else
-    $run_builder ${compile_line} -ldflags "-X $info" -o "bin/$i" "src/mongoshake//$i/main/$i.go"
-  fi
+for i in "${modules[@]}" ; do
+	echo "Build ""$i"
+	if [ $DEBUG -eq 1 ]; then
+		$run_builder ${compile_line} -ldflags "-X $info" -gcflags='-N -l' -o "bin/$i" -tags "debug" "src/mongoshake//$i/main/$i.go"
+	else
+		$run_builder ${compile_line} -ldflags "-X $info" -o "bin/$i" "src/mongoshake//$i/main/$i.go"
+	fi
 
-#   execute and show compile messages
-#  	if [ -f ${output}/"$i" ];then
-#  		${output}/"$i"
-#  	fi
+  if [ $is_cross_compile = false ]; then
+    # execute and show compile messages
+  	if [ -f ${output}/"$i" ];then
+  		${output}/"$i"
+  	fi
+  fi
 done
 
 # *.sh
@@ -86,9 +93,10 @@ cp scripts/stop.sh ${output}/
 cp scripts/mongoshake-stat ${output}/
 cp scripts/comparison.py ${output}/
 
-if [ "Linux" == "$(uname -s)" ]; then
-  # hypervisor
-  gcc -Wall -O3 scripts/hypervisor.c -o ${output}/hypervisor -lpthread
-elif [ "Darwin" == "$(uname -s)" ]; then
-  printf "\\nWARNING !!! MacOS doesn't supply hypervisor\\n"
+
+if [ "Linux" == "$(uname -s)" ];then
+	# hypervisor
+	gcc -Wall -O3 scripts/hypervisor.c -o ${output}/hypervisor -lpthread
+elif [ "Darwin" == "$(uname -s)" ];then
+	printf "\\nWARNING !!! MacOS doesn't supply hypervisor\\n"
 fi
