29ea4c14f2a7aa3ffe6b40a2c50d58893459f782
lydarkforest <linyunads1379@163.com>
2019-11-20 16:24:16 +0800

    update start.sh without hypervisor

--- scripts/start.sh
+++ scripts/start.sh
@@ -12,11 +12,12 @@ fi
 name="collector"
 
 if [ "Darwin" == "$(uname -s)" ];then
-	printf "\\nWARNING !!! MacOs doesn't supply to use this script, please use \"./%s -conf=config_file_name\" manual command to run\\n" "$name"
-    exit 1
+	(./$name.darwin -conf=$1 1>> $name.output 2>&1 &)
+elif [ "Linux" == "$(uname -s)" ];then
+  (./$name.linux -conf=$1 1>> $name.output 2>&1 &)
+else
+  (./$name.windows -conf=$1 1>> $name.output 2>&1 &)
 fi
-
-(./$name -conf=$1 1>> $name.output 2>&1 &)
 
 #GOMAXPROCS=0
 #
