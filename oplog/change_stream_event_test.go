alibaba/MongoShake

commit 810acbf91ce94c3880c2174f25ca42a9fc56e850

[refine]:handle 'update document must contain key beginning with '$'' error

oplog/change_stream_event.go
@@ -240,7 +240,7 @@ func ConvertEvent2Oplog(input []byte, fullDoc bool) (*PartialLog, error) {
 		oplog.Namespace = fmt.Sprintf("%s.%s", ns["db"], ns["coll"])
 		oplog.Operation = "u"
 		oplog.Query = event.DocumentKey
-		oplog.Object = event.FullDocument
+		oplog.Object = bson.D{bson.E{Key: "$set", Value: event.FullDocument}}
 	case "update":
 		/*
 		 * PRIMARY> db.test.find()

.gitignore
@@ -26,6 +26,3 @@ bin/
 /cmd/collector/collector
 /cmd/receiver/receiver
 conf/
-
-#for unit test
-unit_test_common/include.go

cmd/collector/collector.go
@@ -165,7 +165,7 @@ func startup() {
 func selectLeader() {
 	// first of all. ensure we are the Master
 	if conf.Options.MasterQuorum && conf.Options.CheckpointStorage == utils.VarCheckpointStorageDatabase {
-		// election become to Master. keep waiting if we are the candidate. election id is must fixed
+		// election become to Master. keep waiting if we are the candidate. election id must be fixed
 		objectId, _ := primitive.ObjectIDFromHex("5204af979955496907000001")
 		quorum.UseElectionObjectId(objectId)
 		go func() {

collector/configure/check.go
@@ -10,7 +10,7 @@ import (
 	utils "github.com/alibaba/MongoShake/v2/common"
 )
 
-// read the given file and parse the fcv do comparison
+// CheckFcv read the given file and parse the fcv do comparison
 func CheckFcv(file string, fcv int) (int, error) {
 	// read line by line and parse the version
 

common/fcv.go
@@ -6,7 +6,7 @@ var (
 		FeatureCompatibleVersion: 1,
 	}
 	FcvConfiguration = Configuration{
-		CurrentVersion:           10,
+		CurrentVersion:           12,
 		FeatureCompatibleVersion: 10,
 	}
 
@@ -26,7 +26,9 @@ var (
 		7:  "2.4.17", // add filter.oplog.gids
 		8:  "2.4.20", // add special.source.db.flag
 		9:  "2.4.21", // remove incr_sync.worker.oplog_compressor; add incr_sync.tunnel.write_thread, tunnel.kafka.partition_number
-		10: "2.6.4",  // remove full_sync.reader.read_document_count; add full_sync.reader.parallel_thread
+		10: "2.6.4",  // remove full_sync.reader.read_document_count; add full_sync.reader.parallel_thread, incr_sync.reader.fetch_batch_size, skip.nsshardkey.verify
+		11: "2.8.6",  // add incr_sync.fetcher.buffer_size_threshold_in_kb, full_sync.do_not_shard_destination
+		12: "2.8.7",  // add incr_sync.executor.bypass_document_validation
 	}
 )
 
@@ -34,7 +36,6 @@ type Fcv interface {
 	IsCompatible(int) bool
 }
 
-// for checkpoint
 type Checkpoint struct {
 	/*
 	 * version: 0(or set not), MongoShake < 2.4, fcv == 0
@@ -48,7 +49,6 @@ func (c Checkpoint) IsCompatible(v int) bool {
 	return v >= c.FeatureCompatibleVersion && v <= c.CurrentVersion
 }
 
-// for configuration
 type Configuration struct {
 	/*
 	 * version: 0(or set not), MongoShake < 2.4.0, fcv == 0

commit f3ac9edc38553301a0f0cc19cbf565dd61c2cd6c

bugfix: route change stream replace events to ReplaceOne instead of $set update
The "replace" change event was converted to {$set: FullDocument}, which makes the
executor take the UpdateOne path instead of ReplaceOne. This leaves target-only
fields in place (silent source/target divergence) and can hit the immutable _id
error. Pass FullDocument through so doUpdate routes it to ReplaceOne, matching the
update + watch_full_document branch.

Also fix the runOplog test helper to mirror the executor (ReplaceOne when the
object has no "$" operator) and add a regression test asserting a replace removes
fields absent from FullDocument.

Re-applies #867; reverts the regression introduced by 810acbf (#936).

oplog/change_stream_event.go
@@ -240,7 +240,8 @@ func ConvertEvent2Oplog(input []byte, fullDoc bool) (*PartialLog, error) {
 		oplog.Namespace = fmt.Sprintf("%s.%s", ns["db"], ns["coll"])
 		oplog.Operation = "u"
 		oplog.Query = event.DocumentKey
-		oplog.Object = bson.D{bson.E{Key: "$set", Value: event.FullDocument}}
+		// pass FullDocument as-is so the executor routes it to ReplaceOne, not a $set update
+		oplog.Object = event.FullDocument
 	case "update":
 		/*
 		 * PRIMARY> db.test.find()

oplog/change_stream_event_test.go
@@ -379,7 +379,13 @@ func runOplog(data *PartialLog) error {
 		_, err := client.Database(ns[0]).Collection(ns[1]).DeleteOne(context.Background(), data.Object)
 		return err
 	case "u":
-		_, err := client.Database(ns[0]).Collection(ns[1]).UpdateOne(context.Background(), data.Query, data.Object)
+		coll := client.Database(ns[0]).Collection(ns[1])
+		// mirror the executor: $-less object is a full-document replace
+		if FindFiledPrefix(data.Object, "$") {
+			_, err := coll.UpdateOne(context.Background(), data.Query, data.Object)
+			return err
+		}
+		_, err := coll.ReplaceOne(context.Background(), data.Query, data.Object)
 		return err
 	case "c":
 		operation, found := ExtraCommandName(data.Object)
@@ -550,6 +556,53 @@ func TestConvertEvent2Oplog(t *testing.T) {
 		assert.Equal(t, 2, len(all), "should be equal")
 		assert.Equal(t, "1", all["1"]["a"], "should be equal")
 		assert.Equal(t, "20", all["2"]["a"], "should be equal")
+	}
+
+	// test replace removes fields absent from FullDocument
+	{
+		fmt.Printf("TestConvertEvent2Oplog case %d.\n", nr)
+		nr++
+
+		var err error
+		client, err = newMongoClient(testUrl)
+		assert.Equal(t, nil, err, "should be equal")
+
+		err = client.Database("testDb").Drop(context.Background())
+		assert.Equal(t, nil, err, "should be equal")
+
+		eventInsert := Event{
+			OperationType: "insert",
+			FullDocument: bson.D{
+				bson.E{Key: "_id", Value: "3"},
+				bson.E{Key: "a", Value: "1"},
+				bson.E{Key: "extra", Value: "keepme"},
+			},
+			Ns: bson.M{"db": "testDb", "coll": "testColl"},
+		}
+		out, err := bson.Marshal(eventInsert)
+		assert.Equal(t, nil, err, "should be equal")
+		err = runByte(out)
+		assert.Equal(t, nil, err, "should be equal")
+
+		eventReplace := Event{
+			OperationType: "replace",
+			DocumentKey:   bson.D{{"_id", "3"}},
+			FullDocument: bson.D{
+				bson.E{Key: "_id", Value: "3"},
+				bson.E{Key: "a", Value: "2"},
+			},
+			Ns: bson.M{"db": "testDb", "coll": "testColl"},
+		}
+		out, err = bson.Marshal(eventReplace)
+		assert.Equal(t, nil, err, "should be equal")
+		err = runByte(out)
+		assert.Equal(t, nil, err, "should be equal")
+
+		all := getAllDoc("testDb", "testColl")
+		assert.Equal(t, 1, len(all), "should be equal")
+		assert.Equal(t, "2", all["3"]["a"], "should be equal")
+		_, hasExtra := all["3"]["extra"]
+		assert.Equal(t, false, hasExtra, "replace must remove fields absent from FullDocument")
 	}
 
 	// test insert & update & delete